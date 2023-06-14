7c7511428a66a3d80b72ab4eac3d320c7bb95195
박성현 <pjhomg5530@naver.com>
2023-06-14 19:51:13 +0900

    :fire: clear git cache

--- src/main/kotlin/com/example/server/DatabaseFactory.kt
+++ src/main/kotlin/com/example/server/DatabaseFactory.kt
@@ -1,4 +1,9 @@
 package com.example.server
 
+import io.ktor.server.config.*
+
 object DatabaseFactory {
+    fun init(config: ApplicationConfig) {
+        val driverClassName = config.property("storage.driverClassName").getString()
+    }
 }
