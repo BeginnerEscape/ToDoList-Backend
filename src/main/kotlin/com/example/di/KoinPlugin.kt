1e867a04cbe462ae3bfa5021a12557d1df4d32c7
박성현 <pjhomg5530@naver.com>
2023-06-14 20:58:09 +0900

    :sparkles: KoinPlugin.kt 생성

---
+++ src/main/kotlin/com/example/di/KoinPlugin.kt
@@ -0,0 +1,10 @@
+package com.example.di
+
+import io.ktor.server.application.*
+import org.koin.ktor.plugin.Koin
+
+fun Application.installKoin() {
+    install(Koin) {
+        modules(httpClientModule)
+    }
+}
