f9ec65b476b88ecb67ed620ff9441a07606721d8
박성현 <pjhomg5530@naver.com>
2023-06-14 20:13:48 +0900

    :sparkles: DatabaseFactory 생성

--- src/main/kotlin/com/example/server/DatabaseFactory.kt
+++ src/main/kotlin/com/example/server/DatabaseFactory.kt
@@ -1,9 +1,31 @@
 package com.example.server
 
 import io.ktor.server.config.*
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
     fun init(config: ApplicationConfig) {
         val driverClassName = config.property("storage.driverClassName").getString()
+        val jdbcURL = config.property("storage.jdbcURL").getString()
+        val user = config.property("storage.user").getString()
+        val password = config.property("storage.password").getString()
+
+        val database = Database.connect(
+            url = jdbcURL,
+            driver = driverClassName,
+            user = user,
+            password = password
+        )
+
+        transaction(database) {
+//            SchemaUtils.create()
+        }
     }
+
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
 }
