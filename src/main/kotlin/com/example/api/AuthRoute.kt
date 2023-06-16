53dbc61ca0365bb4c58be2ffde50c1f1f5dac266
박성현 <pjhomg5530@naver.com>
2023-06-16 12:04:11 +0900

    :sparkles: AutoRoute 구현

---
+++ src/main/kotlin/com/example/api/AuthRoute.kt
@@ -0,0 +1,46 @@
+package com.example.api
+
+import com.example.domain.model.auth.GAuthUserModel
+import com.example.domain.usecase.auth.GAuthLoginUseCase
+import com.example.domain.usecase.auth.GetUserInfoUseCase
+import com.example.domain.usecase.auth.LogoutUseCase
+import com.example.domain.usecase.auth.ReissueAccessTokenUseCase
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+
+fun Route.autoRoute() {
+    val gAuthLoginUseCase: GAuthLoginUseCase by inject()
+    val getUserInfoUseCase: GetUserInfoUseCase by inject()
+    val logoutUseCase: LogoutUseCase by inject()
+    val reissueAccessTokenUseCase: ReissueAccessTokenUseCase by inject()
+
+    route("/auth") {
+        post {
+            val gAuthUserModel = call.receive<GAuthUserModel>()
+            val tokenItem = gAuthLoginUseCase(GAuthUserModel(gAuthUserModel.email, gAuthUserModel.password))
+            call.respond(tokenItem)
+        }
+
+        get {
+            val accessToken = call.parameters["access_token"] ?: return@get call.respondText("Missing accessToken", status = HttpStatusCode.BadRequest)
+            val userInfo = getUserInfoUseCase(accessToken)
+            call.respond(userInfo)
+        }
+
+        delete {
+            val accessToken = call.parameters["access_token"] ?: return@delete call.respondText("Missing accessToken", status = HttpStatusCode.BadRequest)
+            logoutUseCase(accessToken)
+            call.respondText("Logout correctly", status = HttpStatusCode.Accepted)
+        }
+
+        patch {
+            val refreshToken = call.parameters["refresh_token"] ?: return@patch call.respondText("Missing refreshToken", status = HttpStatusCode.BadRequest)
+            val tokenItem = reissueAccessTokenUseCase(refreshToken)
+            call.respond(tokenItem)
+        }
+    }
+}
