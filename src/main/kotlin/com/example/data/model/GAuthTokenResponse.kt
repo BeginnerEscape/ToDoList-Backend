deee912a9d92bfbe099a9f187c7e9ef59f286ac6
박성현 <pjhomg5530@naver.com>
2023-06-16 12:05:39 +0900

    :sparkles: Serializable 설정

--- src/main/kotlin/com/example/data/model/GAuthTokenResponse.kt
+++ src/main/kotlin/com/example/data/model/GAuthTokenResponse.kt
@@ -1,5 +1,8 @@
 package com.example.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GAuthTokenResponse(
     val accessToken: String,
     val refreshToken: String
--- src/main/kotlin/com/example/data/model/GAuthUser.kt
+++ src/main/kotlin/com/example/data/model/GAuthUser.kt
@@ -1,5 +1,8 @@
 package com.example.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GAuthUser(
     val email: String,
     val password: String
