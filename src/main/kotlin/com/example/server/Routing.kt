9403de20b1d3b494061f2d1e8404f9a4fb6f89f9
박성현 <pjhomg5530@naver.com>
2023-06-16 12:05:50 +0900

    :sparkles: autoRoute 설정

--- src/main/kotlin/com/example/server/Routing.kt
+++ src/main/kotlin/com/example/server/Routing.kt
@@ -1,10 +1,11 @@
 package com.example.server
 
+import com.example.api.autoRoute
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
     routing {
-
+        autoRoute()
     }
 }
