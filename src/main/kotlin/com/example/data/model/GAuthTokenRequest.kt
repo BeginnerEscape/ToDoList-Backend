8d43fc6398d628636f0cc15b378214e5b648f6fe
박성현 <pjhomg5530@naver.com>
2023-06-16 12:04:46 +0900

    :sparkles: 초기값 설정

--- src/main/kotlin/com/example/data/model/GAuthTokenRequest.kt
+++ src/main/kotlin/com/example/data/model/GAuthTokenRequest.kt
@@ -1,8 +1,11 @@
 package com.example.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GAuthTokenRequest(
     val code: String,
-    val clientId: String = "",
-    val clientSecret: String = "",
-    val redirectUri: String = ""
+    val clientId: String = "3c4a659279364866ae1f3b3c5cd0e26e2b08fac490a743b7898264cdcc5ca3d3",
+    val clientSecret: String = "f48db76ba6d045698b26a9791504d6a5965766fb551b49a39b0a67f3a0bce169",
+    val redirectUri: String = "http://localhost:3000"
 )
