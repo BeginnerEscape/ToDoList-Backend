dad6ec73907cf1f9bc74ba691bcde9340d5104d0
박성현 <pjhomg5530@naver.com>
2023-06-16 12:01:50 +0900

    :sparkles: DB에 email, password 추가

--- src/main/kotlin/com/example/domain/model/auth/AuthItem.kt
+++ src/main/kotlin/com/example/domain/model/auth/AuthItem.kt
@@ -7,17 +7,20 @@ import org.jetbrains.exposed.sql.Table
 data class AuthItem(
     val userId: Int,
     val tokenItem: TokenItem,
-    val userItem: UserItem
+    val userItem: UserItem,
+    val gAuthUserModel: GAuthUserModel
 )
 
 object Auths: Table() {
     val userid = integer("user_id").autoIncrement()
-    val accessToken = varchar("access_token", 50)
-    val refreshToken = varchar("refresh_token", 50)
-    val accessTokenExp = varchar("access_token_exp", 20)
-    val refreshTokenExp = varchar("refresh_token_exp", 20)
+    val accessToken = varchar("access_token", 300) // 271
+    val refreshToken = varchar("refresh_token", 300) // 271
+    val accessTokenExp = varchar("access_token_exp", 30) // 27
+    val refreshTokenExp = varchar("refresh_token_exp", 30) // 27
     val grade = varchar("grade", 1)
     val className = varchar("class_name", 1)
+    val email = varchar("email", 20)
+    val password = varchar("password", 20)
 
     override val primaryKey = PrimaryKey(userid)
 }
