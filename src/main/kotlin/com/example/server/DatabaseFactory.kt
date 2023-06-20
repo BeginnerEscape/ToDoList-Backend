d9b6df9f519a9f59e0e6a6fbd6e37083436bdd2b
박성현 <pjhomg5530@naver.com>
2023-06-20 15:47:56 +0900

    :fire: 주석 삭제

--- src/main/kotlin/com/example/server/DatabaseFactory.kt
+++ src/main/kotlin/com/example/server/DatabaseFactory.kt
@@ -10,18 +10,6 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
     fun init(config: ApplicationConfig) {
-//        val driverClassName = config.property("storage.driverClassName").getString()
-//        val jdbcURL = config.property("storage.jdbcURL").getString()
-//        val user = config.property("storage.user").getString()
-//        val password = config.property("storage.password").getString()
-//
-//        val database = Database.connect(
-//            url = jdbcURL,
-//            driver = driverClassName,
-//            user = user,
-//            password = password
-//        )
-
         val driverClassName = "org.h2.Driver"
         val jdbcURL = "jdbc:h2:~/todolist"
         val database = Database.connect(
